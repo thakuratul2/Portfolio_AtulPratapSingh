a6929982ff80253e75d8c8825ac67c4e699f484b
thakuratul2 <pratapsinghatul111@gmail.com>
2023-03-17 22:02:31 +0530

    Project Fully Complete

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,48 +1,11 @@
-import React,{useState, useRef} from 'react'
-import emailjs from "@emailjs/browser";
+import React from 'react'
+
 import { Container, Row, Col } from "react-bootstrap";
 import Button from 'react-bootstrap/Button';
 import './Contact.css'
 
 const Contact = () => {
-    const form = useRef();
-    const [done, setDone] = useState(false)
-    const [notDone, setNotDone] = useState(false)
-    const [formData, setFormData] = useState({});
-
-    const handleChange = (e) => {
-        setFormData({...formData, [e.target.name] : e.target.value})
-        setDone(false)
-        setNotDone(false)
-    }
-
-    const sendEmail = (e) => {
-    e.preventDefault();
-    
-    if(!formData.from_name || !formData.reply_to ||!formData.message){
-      setNotDone(true)
-    } else {
-      
-      //  Please use your own credentials from emailjs or i will recive your email
-      
-    emailjs
-      .sendForm(
-        "service_niilndo",
-        "template_6z5idye",
-        form.current,
-        "VOBt6Akm1LhI5CZG-"
-      )
-      .then(
-        (result) => {
-          console.log(result.text);
-          setDone(true);
-        },
-        (error) => {
-          console.log(error.text);
-        }
-      );
-    }
-    };
+  
     
 
     return(
@@ -53,13 +16,13 @@ const Contact = () => {
             <h1 className="yellow">Contact me</h1>
             </Col>
             <Col md={6} className="c-right">
-                <form ref={form} onSubmit={sendEmail}>
-                <input type="text" name="from_name" className="user"  placeholder="Name" onChange={handleChange}/>
-                <input type="email" name="reply_to" className="user" placeholder="Email" onChange={handleChange} />
-                <textarea name="message" className="user" placeholder="Message" onChange={handleChange} />
-                <span className='not-done' >{notDone && "Please, fill all the input field"}</span>
-                <Button type="submit" className="button" disabled={done}>Send</Button>
-                <span className='done'>{done && "Thanks for Contacting me"}</span>
+                <form action='https://formspree.io/f/mdovdqnl' method='POST'>
+                <input type="text" name="from_name" className="user"  placeholder="Name" required/>
+                <input type="email" name="reply_to" className="user" placeholder="Email" required/>
+                <textarea name="message" className="user" placeholder="Message" required />
+                
+                <Button type="submit" className="button">Send</Button>
+                
                 </form>
             </Col>
             </Row>
